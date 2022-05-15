7fddf97bc4b76a2ab9964e329b29a862a9614e5a
R33TR0 <94188329+R33TR0@users.noreply.github.com>
2022-05-15 12:48:36 +0200

    update

--- js/script.js
+++ js/script.js
@@ -220,8 +220,8 @@ function applyStrings(containers) {
         //find all the elements that have title
         let locale = container.getAttribute('lang');
         //console.log('looking inside of ', locale);
-        container.querySelectorAll(`[name]`).forEach(element => {
-            let key = element.getAttribute('name');
+        container.querySelectorAll(`[data-key]`).forEach(element => {
+            let key = element.getAttribute('data-key');
             //console.log(element);
             //console.log(key);
             let lang = locale.substr(0, 2); //first 2 characters
