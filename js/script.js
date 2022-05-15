2038e3630f8be5f1cd287e0b4435e2bd8bc487b9
R33TR0 <94188329+R33TR0@users.noreply.github.com>
2022-05-15 20:45:00 +0200

    new update

--- js/script.js
+++ js/script.js
@@ -217,14 +217,13 @@ let langdata = {
 
 function applyStrings(containers) {
     containers.forEach(container => {
-        //find all the elements that have title
         let locale = container.getAttribute('lang');
         //console.log('looking inside of ', locale);
         container.querySelectorAll(`[title]`).forEach(element => {
             let key = element.getAttribute('title');
             //console.log(element);
             //console.log(key);
-            let lang = locale.substr(0, 2); //first 2 characters
+            let lang = locale.substr(0, 2);
             if (key) {
                 element.textContent = langdata.languages[lang].strings[key];
             }
@@ -233,16 +232,15 @@ function applyStrings(containers) {
 }
 
 function findLocaleMatch() {
-    let keys = Object.keys(langdata.languages); //from our data
-    let locales = Intl.getCanonicalLocales(keys); //from our data validated
+    let keys = Object.keys(langdata.languages);
+    let locales = Intl.getCanonicalLocales(keys);
 
-    let lang = navigator.language; //from browser 
-    let locale = Intl.getCanonicalLocales(lang); //from browser validated
+    let lang = navigator.language;
+    let locale = Intl.getCanonicalLocales(lang);
     console.log('browser language', lang);
     console.log('locales from data file', locale);
 
-    //find the match for locale inside locales
-    let langMatch = document.documentElement.getAttribute('lang'); //default
+    let langMatch = document.documentElement.getAttribute('lang');
     locales = locales.filter(l => locale == l);
     langMatch = (locales.length > 0) ? locales[0] : langMatch;
     return langMatch;
@@ -256,7 +254,6 @@ function changeLang() {
     console.log("cv: ", document.getElementById("cv").lang);
 
 
-    //skip the lang value in the HTML tag for this example
     let zones = document.querySelectorAll('html [lang]');
     applyStrings(zones);
 
